c2cc8b8c6a08ba0ead24bdfa5940a21ff7dcf1a5
SergioCADGIS <89920019+SergioCadGis@users.noreply.github.com>
2024-08-28 17:31:17 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**Classe Main
+  *@author Sergio Pedro
+  *@version 1.0
+  *since Release 28/08/2024
+  */
+
+public class Main {
+  int numero = 1021;
+  String agencia = "067-8";
+  String nomeCliente = "Sergio Pedro";
+  double saldo = 237.12;
+
+  public static void main(String[] args) {
+      Main minhaConta = new Main();
+
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+      // Solicitando os dados ao usuário
+      System.out.print("Digite o número da conta: ");
+       minhaConta.numero = scanner.nextInt();
+
+      System.out.print("Digite a agencia da conta: ");
+       minhaConta.agencia = scanner.next();
+      
+      System.out.print("Digite seu nome: ");
+       minhaConta.nomeCliente = scanner.next();
+  
+      //System.out.print("Digite seu Saldo: ");
+      //minhaConta.saldo = scanner.nextDouble();
+    
+    //System.out.println("Digite o Número da Conta: " + minhaConta.numero);
+    //System.out.println("Digite oAgência: " + minhaConta.agencia);
+    //System.out.println("Digite seu Nome: " + minhaConta.nomeCliente);
+    //System.out.println("Saldo: R$ " + minhaConta.saldo);
+
+    System.out.println("Olá " + minhaConta.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + minhaConta.agencia  + ", conta " + minhaConta.numero + " e seu saldo " +  minhaConta.saldo + " já está disponível para saque!");
+
+  }
+  
+  
+
+}
